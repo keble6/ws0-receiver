b295355606e9839d88ac9fd7adae01229ddcd53e
Rob Wilson <rob@rwilson.org.uk>
2022-03-31 10:09:52 +0100

    Add comments

--- main.ts
+++ main.ts
@@ -1,19 +1,25 @@
+// Simple test
 input.onButtonPressed(Button.A, function () {
     serial.writeLine("Testing!")
 })
 radio.onReceivedString(function (receivedString) {
-    // Is this the dateTime message? If so then don't send "Enter"
+    // Is this the first (dateTime) message? If so then DON'T send "Enter" after the message
+    // The first message has a comma at the end
     if (receivedString.charAt(receivedString.length - 1) == ",") {
         serial.writeString(receivedString)
     } else {
+        // It's the second message, so DO send "Enter" after the message
         serial.writeLine(receivedString)
     }
     serial.writeString("")
 })
+// Send a radio message to start upload
 input.onButtonPressed(Button.B, function () {
     radio.sendString("start")
     basic.showString("start")
 })
-serial.writeLine("1")
-radio.setGroup(1)
+// Set our team number!
+let team = 1
+serial.writeLine("" + (team))
+radio.setGroup(team)
 radio.setTransmitPower(7)
